259287092501bd013a65186e95166948f67cfbf2
ichikawa-az <138674830+ichikawa-az@users.noreply.github.com>
2023-09-04 21:38:14 +0900

    kadai_007を提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body> 
+  <p>
+    <?php
+    $info = ['name'=>'onion', 'price'=>200, 'weight'=>160];
+
+    echo '<br>';
+
+    print_r($info);
+
+    ?>
+  </p>
+
+</body>
+
+</html>
